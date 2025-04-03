8717c8daea933579588693d721db53bda4433f87
JaloliddinGofurov <jaloliddingofurov.smh@gmail.com>
2025-04-03 11:34:06 +0500

    Add localhost

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 const elSiteThemeToggler = document.querySelector('.site-header-theme-toggler');
 
-elSiteThemeToggler.addEventListener('click', function () {
+// Oldin saqlangan rejimni tekshirish
+if (localStorage.getItem('theme') === 'dark') {
   document.body.classList.add('dark-mode');
+}
+
+// Tugmani bosganda rejimni o'zgartirish
+elSiteThemeToggler.addEventListener('click', function () {
+  if (document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light'); // Yorug' rejimni saqlash
+  } else {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark'); // Qorong'u rejimni saqlash
+  }
 });
